86c645810844f27409111103ffea4e23e0a88ef0
xdechamps <x.dechamps@students.ephec.be>
2016-10-22 17:42:55 +0200

    Test LUIS Sample

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
+    appId: "luisTestAppHackathon",
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
