62d499dc058af6c9263818661f3925c2a80b576b
xdechamps <x.dechamps@students.ephec.be>
2016-10-22 16:52:38 +0200

    Test LUIS Sample

--- app.js
+++ app.js
@@ -1,135 +1,117 @@
-/*-----------------------------------------------------------------------------
-A speech to text bot for the Microsoft Bot Framework. 
------------------------------------------------------------------------------*/
-
-// This loads the environment variables from the .env file
-require('dotenv-extended').load();
-
-const builder = require("botbuilder"),
-    fs = require("fs"),
-    needle = require("needle"),
-    restify = require("restify"),
-    request = require("request"),
-    speechService = require("./speech-service.js"),
-    url = require("url");
-
-//=========================================================
-// Bot Setup
-//=========================================================
+var builder = require('botbuilder');
+var restify = require('restify');
+var Store = require('./store');
 
 // Setup Restify Server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3979, () => {
-    console.log("%s listening to %s", server.name, server.url);
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat bot
-const connector = new builder.ChatConnector({
+var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
-const bot = new builder.UniversalBot(connector);
-server.post("/api/messages", connector.listen());
+// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+const LuisModelUrl = process.env.LUIS_MODEL_URL ||
+    'https://api.projectoxford.ai/luis/v1/application?id=162bf6ee-379b-4ce4-a519-5f5af90086b5&subscription-key=11be6373fca44ded80fbe2afa8597c18';
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
+// Main dialog with LUIS
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+    .matches('SearchHotels', [
+        function (session, args, next) {
+            session.send('Welcome to the Hotels finder! we are analyzing your message: \'%s\'', session.message.text);
 
-bot.dialog("/", session => {
-    if (hasAudioAttachment(session)) {
-        var stream = getAudioStreamFromAttachment(session.message.attachments[0]);
-        speechService.getTextFromAudioStream(stream)
-            .then(text => {
-                session.send(processText(session.message.text, text));
-            })
-            .catch(error => {
-                session.send("Oops! Something went wrong. Try again later.");
-                console.error(error);
-            });
-    }
-    else {
-        session.send("Did you upload an audio file? I'm more of an audible person. Try sending me a wav file");
-    }
-});
-
-//=========================================================
-// Utilities
-//=========================================================
-const hasAudioAttachment = session => {
-    return ((session.message.attachments.length > 0) && ((session.message.attachments[0].contentType === "audio/wav") || (session.message.attachments[0].contentType === "application/octet-stream")));
-}
-
-const getAudioStreamFromAttachment = attachment => {
-    var headers = {};
-    if (isSkypeAttachment(attachment)) {
-        // The Skype attachment URLs are secured by JwtToken,
-        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
-        // https://github.com/Microsoft/BotBuilder/issues/662
-        connector.getAccessToken((error, token) => {
-            var tok = token;
-            headers['Authorization'] = 'Bearer ' + token;
-            headers['Content-Type'] = 'application/octet-stream';
-
-            return needle.get(attachment.contentUrl, { headers: headers });
-        });
-    }
-
-    headers['Content-Type'] = attachment.contentType;
-    return needle.get(attachment.contentUrl, { headers: headers });
-}
-
-const isSkypeAttachment = attachment => {
-    if (url.parse(attachment.contentUrl).hostname.substr(-"skype.com".length) == "skype.com") {
-        return true;
-    }
-
-    return false;
-}
-
-const processText = (input, text) => {
-    var result = "You said: " + text + ".";
-
-    if (input.toUpperCase().trim() == "WORD") {
-        const wordCount = text.split(" ").filter(x => x).length;
-        result += " Word Count: " + wordCount;
-    }
-    else if (input.toUpperCase().trim() == "CHARACTER") {
-        const characterCount = text.replace(/ /g, "").length;
-        result += " Character Count: " + characterCount;
-    }
-    else if (input.toUpperCase().trim() == "SPACE") {
-        const spaceCount = (text.split(" ").length - 1);
-        result += " Space Count: " + spaceCount;
-    }
-    else if (input.toUpperCase().trim() == "VOWEL") {
-        const m = text.match(/[aeiou]/gi);
-        const vowelCount =  m === null ? 0 : m.length;
-        result += " Vowel Count: " + vowelCount;
-    }
-    else if (input) {
-        const keywordCount = text.toUpperCase().split(" ").filter(x => x == input.toUpperCase()).length;
-        result += " Keyword " + input + " found " + keywordCount + " times.";
-    }
-
-    return result;
-}
-
-//=========================================================
-// Bots Events
-//=========================================================
-
-// Sends greeting message when the bot is first added to a conversation
-bot.on("conversationUpdate", message => {
-    if (message.membersAdded) {
-        message.membersAdded.forEach(identity => {
-            if (identity.id === message.address.bot.id) {
-                const reply = new builder.Message()
-                    .address(message.address)
-                    .text("Hi! I am SpeechToText Bot. I can understand the content of any audio and convert it to text. Try sending me a wav file.");
-                bot.send(reply);
-                return;
+            // try extracting entities
+            var cityEntity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.geography.city');
+            var airportEntity = builder.EntityRecognizer.findEntity(args.entities, 'AirportCode');
+            if (cityEntity) {
+                // city entity detected, continue to next step
+                session.dialogData.searchType = 'city';
+                next({ response: cityEntity.entity });
+            } else if (airportEntity) {
+                // airport entity detected, continue to next step
+                session.dialogData.searchType = 'airport';
+                next({ response: airportEntity.entity });
+            } else {
+                // no entities detected, ask user for a destination
+                builder.Prompts.text(session, 'Please enter your destination');
             }
-        });
-    }
-});
+        },
+        function (session, results) {
+            var destination = results.response;
+
+            var message = 'Looking for hotels';
+            if (session.dialogData.searchType === 'airport') {
+                message += ' near %s airport...';
+            } else {
+                message += ' in %s...';
+            }
+
+            session.send(message, destination);
+
+            // Async search
+            Store
+                .searchHotels(destination)
+                .then((hotels) => {
+                    // args
+                    session.send('I found %d hotels:', hotels.length);
+
+                    var message = new builder.Message()
+                        .attachmentLayout(builder.AttachmentLayout.carousel)
+                        .attachments(hotels.map(hotelAsAttachment));
+
+                    session.send(message);
+
+                    // End
+                    session.endDialog();
+                });
+        }
+    ])
+    .matches('ShowHotelsReviews', (session, args) => {
+        // retrieve hotel name from matched entities
+        var hotelEntity = builder.EntityRecognizer.findEntity(args.entities, 'Hotel');
+        if (hotelEntity) {
+            session.send('Looking for reviews of \'%s\'...', hotelEntity.entity);
+            Store.searchHotelReviews(hotelEntity.entity)
+                .then((reviews) => {
+                    var message = new builder.Message()
+                        .attachmentLayout(builder.AttachmentLayout.carousel)
+                        .attachments(reviews.map(reviewAsAttachment));
+                    session.send(message)
+                });
+        }
+    })
+    .matches('Help', builder.DialogAction.send('Hi! Try asking me things like \'search hotels in Seattle\', \'search hotels near LAX airport\' or \'show me the reviews of The Bot Resort\''))
+    .onDefault((session) => {
+        session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+    });
+
+bot.dialog('/', intents);
+
+// Helpers
+function hotelAsAttachment(hotel) {
+    return new builder.HeroCard()
+        .title(hotel.name)
+        .subtitle('%d stars. %d reviews. From $%d per night.', hotel.rating, hotel.numberOfReviews, hotel.priceStarting)
+        .images([new builder.CardImage().url(hotel.image)])
+        .buttons([
+            new builder.CardAction()
+                .title('More details')
+                .type('openUrl')
+                .value('https://www.bing.com/search?q=hotels+in+' + encodeURIComponent(hotel.location))
+        ]);
+}
+
+function reviewAsAttachment(review) {
+    return new builder.ThumbnailCard()
+        .title(review.title)
+        .text(review.text)
+        .images([new builder.CardImage().url(review.image)])
+}
